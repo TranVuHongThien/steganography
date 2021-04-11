0e9c92925427fb8616f5f69e400e26b9ace59524
thien.tran1011@hcmut.edu.vn <thien.tran1011@hcmut.edu.vn>
2021-04-11 14:34:52 +0700

    update imghide.py

--- imghide.py
+++ imghide.py
@@ -199,14 +199,6 @@ def test(img):
 	print(pix[0])
 	print(type(pix))
 
-# def print_credits():
-# 	table = Table(show_header=True)
-# 	table.add_column("Author",style="yellow")
-# 	table.add_column("Contact",style="yellow")
-# 	table.add_row("Teja Swaroop", "tejaswaroop156@gmail.com ")
-# 	console.print(table)
-
-
 
 def insertHeaders(img):
 	
